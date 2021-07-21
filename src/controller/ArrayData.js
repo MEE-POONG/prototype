8be31263ffad63f72506a66306971ded13264625
devilzeros <devilzeros@outlook.co.th>
2021-07-21 11:32:16 +0700

    est

---
+++ src/controller/ArrayData.js
@@ -0,0 +1,123 @@
+var FoodMenuTH = [
+  {
+    text: "NO.",
+    css: "first",
+  },
+  {
+    text: "Image",
+    css: "image",
+  },
+  {
+    text: "NameTH",
+    css: "name",
+  },
+  {
+    text: "NameEN",
+    css: "name",
+  },
+  {
+    text: "Manager",
+    css: "manager",
+  },
+];
+var FoodMenuTD = [
+  {
+    id: "FM0001",
+    img: "default.jpg",
+    nameTH: "Minerva Hooper",
+    nameEN: "Doris Greene	",
+    star: true,
+  },
+  {
+    id: "FM0002",
+    img: "default.jpg",
+    nameTH: "Jacob",
+    nameEN: "Doris Greene	",
+    star: true,
+  },
+  {
+    id: "FM0003",
+    img: "default.jpg",
+    nameTH: "Sage Rodriguez	",
+    nameEN: "Doris Greene	",
+    star: true,
+  },
+  {
+    id: "FM0004",
+    img: "default.jpg",
+    nameTH: "Philip Chaney	",
+    nameEN: "Doris Greene	",
+    star: true,
+  },
+  {
+    id: "FM0005",
+    img: "default.jpg",
+    nameTH: "first",
+    nameEN: "first",
+    star: false,
+  },
+];
+var GalleryArray = [
+  {
+    img: "default.jpg",
+  },
+  {
+    img: "default.jpg",
+  },
+  {
+    img: "default.jpg",
+  },
+  {
+    img: "default.jpg",
+  },
+  {
+    img: "default.jpg",
+  },
+];
+const MenuArray = [
+  {
+    src: require("../assets/img/bn-h.jpg"),
+    altText: "",
+    text: "",
+    detail: "",
+  },
+];
+const ProductArray =[
+  {
+    src: require("../assets/img/Somdej/s1.jpg"),
+    text: "พระสมเด็จ",
+    detail: "รายละเอียด",
+  },
+  {
+    src: require("../assets/img/Somdej/s1.jpg"),
+    text: "พระสมเด็จ",
+    detail: "รายละเอียด",
+  },
+  {
+    src: require("../assets/img/Somdej/s1.jpg"),
+    text: "พระสมเด็จ",
+    detail: "รายละเอียด",
+  },
+  {
+    src: require("../assets/img/Somdej/s1.jpg"),
+    text: "พระสมเด็จ",
+    detail: "รายละเอียด",
+  },
+  {
+    src: require("../assets/img/Somdej/s1.jpg"),
+    text: "พระสมเด็จ",
+    detail: "รายละเอียด",
+  },
+  {
+    src: require("../assets/img/Somdej/s1.jpg"),
+    text: "พระสมเด็จ",
+    detail: "รายละเอียด",
+  },
+]
+module.exports = {
+  FoodMenuTH,
+  FoodMenuTD,
+  GalleryArray,
+  MenuArray,
+  ProductArray,
+};
